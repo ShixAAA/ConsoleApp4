7d4757131f4d53cfdd54e1a1c9a6645f99a7fbf4
test05 <test5@user.com>
2021-10-05 12:15:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double H, z, s, R, a; //объяление переменных
+            //Присваивание значения переменных
+            a = 2;
+            R = 3;
+            //Вычисление промежуточных результатов
+            s = Math.PI * Math.Pow(R, 2);
+            H = 2 * a;
+            //Вычисление результата
+            z = Math.PI * R * H + s;
+            Console.WriteLine("z={0:##.####}",z);//Округление числа до тысячных
+            Console.ReadKey();
+
+        }
+    }
+}
